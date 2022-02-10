9e34cf0ee561f8b826d6e0270eed8a4a906b9690
mamradrohlikii <82773992+mamradrohlikii@users.noreply.github.com>
2022-02-10 06:29:21 +0100

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+
+echo MAMRADROHLIKI INSTALLER
+
+pkg update 
+pkg install python
+pkg install python2
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+pkg install git
+git clone https://github.com/hk4crprasad/Evil-sms
+git clone
+
+echo DONE BASIC SETUP
